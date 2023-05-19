9e6fac258b0f33b049971fd3967ac72046a61dae
Justin MBP M1 <justintankh@gmail.com>
2023-05-19 22:18:07 +0800

    feat: add database setup to CI

--- src/test/java/com/geometrypuzzle/backend/ApplicationTests.java
+++ src/test/java/com/geometrypuzzle/backend/ApplicationTests.java
@@ -1,17 +1,12 @@
 package com.geometrypuzzle.backend;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
 	@Test()
-	@EnabledOnOs({OS.MAC, OS.WINDOWS})
-		/* To mock disabled on github action
-	* - For future  of spring profiles */
 	void contextLoads() {
 	}
 
